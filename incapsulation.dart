b6b898ef35f0404c7ed0b9c1e7c40d9ef0cbd186
Md Imran Ali <mdimranali748244@gmail.com>
2026-07-02 09:44:06 +0600

    Highlight Comment

--- incapsulation.dart
+++ incapsulation.dart
@@ -2,23 +2,36 @@ class Student {
   String? _name;
   int? _age;
 
-//setters methods
+  //setters methods
   void setName(String name) {
     _name = name;
   }
+
   void setAge(int age) {
     _age = age;
   }
 
+  // //getters methods
+  // String? getName() {
+  //   return _name;
+  // }
+  // int? getAge() {
+  //   return _age;
+  // }
 
   void showInfo() {
+    //setters methods
     print('name : $_name');
     print('age : $_age');
+    //getters methods
+    // print('name : ${getName()}');
+    // print('age : ${getAge()}');
   }
 }
+
 void main() {
   Student demo = Student();
-  demo._name = 'Imran';
-  demo._age = 30;
+  demo.setName('Imran');
+  demo.setAge(30);
   demo.showInfo();
 }
